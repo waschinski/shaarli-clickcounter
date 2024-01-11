abf031f94e2bdec8f427c3eb7bddd05683668021
root <root@metaref.link>
2024-01-11 23:38:45 +0000

    Initial commit

---
+++ clickcounter/ClickcounterController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shaarli\Plugin\Clickcounter;
+
+use Shaarli\Front\Controller\Admin\ShaarliAdminController;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Shaarli\Helper\FileUtils;
+
+class ClickcounterController extends ShaarliAdminController
+{
+    /**
+     * GET /plugin/clickcounter/:id
+     */
+    public function index(Request $request, Response $response, array $args): Response
+    {
+	if (!array_key_exists('id', $args) || !$this->container->bookmarkService->exists((int) $args['id'])) {
+            $this->saveErrorMessage('Invalid ID provided.');
+            return $this->redirectFromReferer($request, $response, ['clickcounter']);
+	}
+
+	$clickData = FileUtils::readFlatDB($this->container->conf->get('resource.data_dir') . '/clickcounter.php', []);
+	if (!array_key_exists($args['id'], $clickData)) {
+            $clickData[$args['id']] = 1;
+	}
+	else {
+            $clickData[$args['id']]++;
+	}
+	FileUtils::writeFlatDB($this->container->conf->get('resource.data_dir') . '/clickcounter.php', $clickData);
+
+	$bookmark = $this->container->bookmarkService->get((int) $args['id']);
+        return $response->withRedirect($bookmark->getUrl());
+    }
+}
---
+++ clickcounter/clickcounter.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Shaarli clickcounter Plugin
+ *
+ * @author Andreas Waschinski
+ * @link 
+ */
+
+require_once __DIR__ . '/ClickcounterController.php';
+
+use Shaarli\Config\ConfigManager;
+use Shaarli\Plugin\PluginManager;
+use Shaarli\Helper\FileUtils;
+
+function clickcounter_register_routes(): array
+{
+    return [
+        [
+	    'method' => 'GET',
+	    'route' => '/{id}',
+	    'callable' => 'Shaarli\Plugin\Clickcounter\ClickcounterController:index',
+	],
+    ];
+}
+
+function hook_clickcounter_render_linklist(array $data, ConfigManager $conf): array
+{
+    foreach ($data['links'] as &$value) {
+        if (strpos($value['real_url'], 'http') === 0) {
+            $value['real_url'] = $data['_BASE_PATH_'] . '/plugin/clickcounter/' . $value['id'];
+	}
+	$clickData = FileUtils::readFlatDB($conf->get('resource.data_dir') . '/clickcounter.php', []);
+	if (array_key_exists($value['id'], $clickData)) {
+            $value['link_plugin'][] = $clickData[$value['id']] . ' clicks';
+	}
+    }
+    return $data;
+}
+
+function hook_clickcounter_render_picwall(array $data, ConfigManager $conf): array
+{
+    foreach ($data['linksToDisplay'] as &$value) {
+        if (strpos($value['real_url'], 'http') === 0) {
+            $value['real_url'] = $data['_BASE_PATH_'] . '/plugin/clickcounter/' . $value['id'];
+        }
+    }
+    return $data;
+}
+
+function hook_clickcounter_render_daily(array $data, ConfigManager $conf): array
+{
+    foreach ($data['linksToDisplay'] as &$value) {
+        if (strpos($value['real_url'], 'http') === 0) {
+            $value['real_url'] = $data['_BASE_PATH_'] . '/plugin/clickcounter/' . $value['id'];
+        }
+    }
+    return $data;
+}
+
+function hook_clickcounter_delete_link(array $data, ConfigManager $conf)
+{
+    $clickData = FileUtils::readFlatDB($conf->get('resource.data_dir') . '/clickcounter.php', []);
+    if (array_key_exists($data['id'], $clickData)) {
+        unset($clickData[$data['id']]);
+        FileUtils::writeFlatDB($conf->get('resource.data_dir') . '/clickcounter.php', $clickData);
+    }
+}
+
